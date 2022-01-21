c1bb574f739964bd58bd67e12ce052f4edb77dfd
srilumpa <marcandre.doll@gmail.com>
2022-01-21 14:26:14 +0100

    Handle multipaging and stop using multistream event emission

--- lib/fluent/plugin/in_shodan_search.rb
+++ lib/fluent/plugin/in_shodan_search.rb
@@ -7,17 +7,25 @@ module Fluent::Plugin
 
     desc "The Shodan query to execute"
     config_param :query, :string
+    desc "The maximum amount of pages to crawl. A 0 or negative value means to crawl all pages"
+    config_param :max_pages, :integer, default: 1
 
     private
 
     def run
-      es = Fluent::MultiEventStream.new
+      log.debug "Starting Shodan query", query: @query, max_pages: @max_pages
       es_time = Fluent::EventTime.now
-      result = @client.host_search(@query)
-      result['matches'].each do |rec|
-        es.add es_time, rec
+      current_page = 0
+      read_entries = 0
+      loop do
+        current_page += 1
+        result = @client.host_search(@query, page: current_page)
+        result['matches'].each do |rec|
+          router.emit(@tag, es_time, rec)
+        end
+        read_entries += result['matches'].length
+        break if (@max_pages >= 0 && current_page >= @max_pages) || read_entries >= result['total']
       end
-      router.emit_stream(@tag, es)
     rescue RuntimeError => re
       log.error "Unable to execute Shodan query", query: @query, page: current_page, error: re
     rescue => exception
